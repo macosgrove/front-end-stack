a9e34e8149cb071e69aef9d94dfa01f8eee3503d
Mary-Anne Cosgrove <mary-anne.cosgrove@envato.com>
2014-09-22 16:58:50 +1000

    Fix build

--- spec/view_spec.rb
+++ spec/view_spec.rb
@@ -3,6 +3,6 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe 'Home' do
   it "displays the README" do
     get "/"
-    expect(last_response.body).to include("Front-End Stack")
+    expect(last_response.body).to include("Graphbox")
   end
 end
