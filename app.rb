65bd7c97b813a3691d2e1ed12cb80d33826f4ce3
Mary-Anne Cosgrove <mary-anne.cosgrove@envato.com>
2014-09-22 11:36:22 +1000

    Not using images yet. Update bundle

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Application
   assets {
     serve '/js', from: 'assets/scripts'
     serve '/css', from: 'assets/stylesheets'
-    serve '/images', from: 'assets/images'
+    # serve '/images', from: 'assets/images'
 
     # The second parameter defines where the compressed version will be served.
     # (Note: that parameter is optional, AssetPack will figure it out.)
